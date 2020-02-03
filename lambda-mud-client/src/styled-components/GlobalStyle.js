fc61289359b2c76ca2fb3358594e2ab4c60aa984
Jonathan Ho <johnnyho82@gmail.com>
2020-02-03 13:16:34 -0500

    Add fonts

--- lambda-mud-client/src/styled-components/GlobalStyle.js
+++ lambda-mud-client/src/styled-components/GlobalStyle.js
@@ -1,14 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Roboto|Major+Mono+Display|VT323&display=swap');
 
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     max-width: 100%;
-    font-family: 'Roboto', sans-serif;
+    font-family: 'VT323', sans-serif;
+    letter-spacing: 0.3rem;
   }
 
   html {
